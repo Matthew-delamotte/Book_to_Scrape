1e6546b6497fbc741ce3095a34c92b0e4a4fddb0
Matthew-delamotte <delamotte.matthew@gmail.com>
2021-04-19 02:17:10 +0200

    Ajout commentaire + input fichier de sauvegarde
    Ajout de commentaire
    Ajout demande de l'adresse de sauvegarde des fichier telécharger

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-# ____Passage branch DEV_________
+
 import string
 import requests
 from bs4 import BeautifulSoup
@@ -7,116 +7,10 @@ import re
 import csv
 import os
 
-
-def clean_filename(filename):
-    var = "-_.() %s%s" % (string.ascii_letters, string.digits)
-    return ''.join(c for c in filename if c in var)
-
-# import time
-#
-#
-# # url = 'http://books.toscrape.com/index.html'
-# # response = requests.get(url)
-#
-# # if response.ok:
-# #     # print(response.headers)
-# #     # print(response.text)
-# #     links = []
-# #     soup = BeautifulSoup(response.text, 'lxml')
-# #     # title = soup.find('title') # --> recupere le premier text dans les balise css title
-# #     trs = soup.findAll('article') # --> recupere tout les text dans les balise css t
-# #     # print(len(trs)) # --> Affiche le nombre de resultat
-# #     # print(str(trs) + '\n') # for tr in trs]
-# #
-# #     # for i in trs:
-# #     #     a = i.find('a')
-# #     #     link = a['href']
-# #     #     links.append('http://books.toscrape.com/' + link) # recupere et contatene les liens
-# #     # print(links)
-#
-# #
-# # url = 'http://books.toscrape.com/index.html'
-# # response = requests.get(url)
-# # links = []
-# #
-# # # time.sleep(1) #--> fait une pause de 1sec
-# #
-# # if response.ok:
-# #     for i in range(1,51):
-# #         print(url)
-# #         url = 'http://books.toscrape.com/catalogue/page-' + str(i + 1) + '.html'
-# #         response = requests.get(url)
-# #         if response.ok:
-# #             soup = BeautifulSoup(response.text, 'lxml')
-# #             trs = soup.findAll('article')
-# #             for i in trs:
-# #                 a = i.find('a')
-# #                 link = a['href']
-# #                 links.append('http://books.toscrape.com/' + link) # recupere et contatene les liens
-# #
-# # with open('fichier.txt', 'w') as file:
-# #     for link in links:
-# #         file.write(link + '\n')
-# #
-# # with open('fichier.txt', 'r') as file:
-# #     for raw in file:
-# #         print(raw)
-# #
-# # url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-# # response = requests.get(url)
-# # if response.ok:
-# #     soup = BeautifulSoup(response.text)
-# #     tableau = soup.find('table', {'class': 'table table-striped'})
-#
-# rel_soup = BeautifulSoup('<p>Back to the <a rel="index">homepage</a></p>', 'html.parser')
-# rel_soup.a['rel']
-# # ['index']
-# rel_soup.a['rel'] = ['index', 'contents']
-# print(rel_soup.p)
-#
-#
-# list1 = [1, 2, 3]
-# str1 = ''.join(str(e) for e in list1)
-# print(str1)
-#
-# import re
-#
-# string = 'Instock(22available)'
-# nombres = re.findall('\d+', string) # recherche tous les décimals
-# print(nombres) # ['34', '65']
-
-# # Add star-rating - WIP
-# rating = soup.find('div', {'class': 'col-sm-6 product_main'}).findNext('p').findNext('p').findNext('p')
-# rating = rating['class']
-# # rating = path.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]')
-# # css_soup = BeautifulSoup('<p class="five star"></p>', 'html.parser')
-# # f = css_soup.a['class']
-# # print(f) #content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating.Five
-# # rating = path.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]')
-# product_out.write('Star Rating\n')
-# product_out.write(rating[1] + ' Star(s)\n\n')
-
-# url = 'http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html'
-# response = requests.get(url)
-# path = html.fromstring(response.content)
-#
-# if response.status_code != 200:
-# 	print("Error fetching page")
-# 	exit()
-# else:
-# 	content = response.content
-# #
-# soup = BeautifulSoup(response.content, 'html.parser')
-# # nb_links = len(soup.find_all('a'))
-# # print(f"There are {nb_links} links in this page")
-#
-# r = soup.find['class']
-# print(soup.find['class'])
-# # f = soup.find()
-# # rel_soup = BeautifulSoup(t, 'html.parser')
-# # f = rel_soup.p['class']
-# # print(' '.join(str(e) for e in f))
-
+# definition fonction
+def clean_filename(filename):  # methode de nettoyage du text de tout les carractére non pris en charge
+    var = "-_.() %s%s" % (string.ascii_letters, string.digits) # recupere les caracter autorisé/caractere/chiffre ascii
+    return ''.join(c for c in filename if c in var) # Join le tout dans un string et return la string
 
 # permanante variable
 
@@ -124,6 +18,7 @@ category_list = []
 url_list = []
 data = {}
 
+path_demand = input("Entrer l'adresse du repertoire de sauvegarde: ")
 
 # add url website
 url_website = 'http://books.toscrape.com/'
@@ -170,15 +65,13 @@ for category_link, value in data.items():
             book_links.append('http://books.toscrape.com/catalogue/' + link) # Found all book of page and add it to list
 
     print(value + '______________________________.......')
-    path = 'D:\\Project Files\\Projet_2\\Book_to_Scrape\\csv_folder' + '\\' + value
-    p = 'D:\\Project Files\\Projet_2\\Book_to_Scrape\\csv_folder' + '\\' + value + '\\'
+    path = path_demand + '\\book_to_scrap_csv\\' + value
+    p = path_demand + '\\book_to_scrap_csv\\' + value + '\\image\\'
     filepath = os.path.join(path, value + '.csv')
     if not os.path.exists(path):
         os.makedirs(path)
-    image_url_list = []
-    title_list = []
     # console check if category name is good
-    with open(filepath, 'w+', encoding="utf-8", newline='') as csv_file:           # create csv file of value name (write mode)
+    with open(filepath, 'w+', encoding="utf-8", newline='') as csv_file:   # create csv file of value name (write mode)
         fieldnames = ['Product_page_url', 'Universal_ product_code', 'Title', 'Price_including_tax',
                       'Price_excluding_tax', 'Number_available', 'Product_description',
                       'Category', 'Review_rating', 'Image_url']         # add field names to csv file
@@ -232,6 +125,8 @@ for category_link, value in data.items():
                 wr.writerow(extract_info)
 
                  # Download image of book url
+                if not os.path.exists(p):
+                    os.makedirs(p)
                 req = requests.get(image_url, stream=True)
                 with open(p + clean_filename(title.text) + '.' + image_url.split('.')[-1], 'wb+') as img_dl: # Split l'url avec tout les '.', et commence la chaine par la dernier occurence.
                     img_dl.write(req.content)
